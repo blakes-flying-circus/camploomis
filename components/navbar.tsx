58bb96465a561ef02ad3d67a92a305a321178adc
“Blake <blake.thompson93@gmail.com>
2023-12-29 18:28:40 +0900

    Fixing navbar to work with md and sm viewports, adding favicons for fun.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+import React, { useState } from 'react';
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -6,37 +8,61 @@ import {
   NavbarBrand,
   NavbarItem,
   NavbarMenuItem,
-} from '@nextui-org/navbar';
-import { link as linkStyles } from '@nextui-org/theme';
+  link as linkStyles,
+} from '@nextui-org/react';
 import NextLink from 'next/link';
 import clsx from 'clsx';
 
 import { siteConfig } from '@/config/site';
 
-export const Navbar = () => (
-  <NextUINavbar maxWidth="xl" position="sticky">
-    <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-      <NavbarBrand as="li" className="gap-3 max-w-fit">
-        <NextLink className="flex justify-start items-center gap-1" href="/">
-          <p className="font-bold text-inherit">CAMP LOOMIS</p>
-        </NextLink>
-      </NavbarBrand>
-      <ul className="hidden lg:flex gap-4 justify-start ml-2">
+export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const logoString = 'CAMP LOOMIS';
+
+  return (
+    <NextUINavbar onMenuOpenChange={setIsMenuOpen} maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <p className="font-bold text-inherit">{logoString}</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden md:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: 'foreground' }),
+                  'data-[active=true]:text-primary data-[active=true]:font-medium'
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+      <NavbarMenu>
         {siteConfig.navItems.map((item) => (
-          <NavbarItem key={item.href}>
+          <NavbarMenuItem key={item.label}>
             <NextLink
               className={clsx(
                 linkStyles({ color: 'foreground' }),
                 'data-[active=true]:text-primary data-[active=true]:font-medium'
               )}
-              color="foreground"
               href={item.href}
             >
               {item.label}
             </NextLink>
-          </NavbarItem>
+          </NavbarMenuItem>
         ))}
-      </ul>
-    </NavbarContent>
-  </NextUINavbar>
-);
+      </NavbarMenu>
+    </NextUINavbar>
+  );
+};
